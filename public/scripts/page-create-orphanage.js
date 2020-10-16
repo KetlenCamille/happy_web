a2ed243f1a826371b3c503177751bbf18af34880
KetlenCamille <ketlen.rieper@gmail.com>
2020-10-16 14:13:13 -0300

    Aula 03 - Criando validação e clone do campo de Fotos / Colocando valor para os buttons Sim e Não

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -28,3 +28,43 @@ map.on('click', (event) => {
     marker = L.marker([lat, lng], { icon })
     .addTo(map)
 })
+
+//Add photo field
+function addPhotoField(){
+    const container = document.querySelector('#images')
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    const newFieldContainer = fieldsContainer[fieldsContainer.length -1].cloneNode(true)
+    const input = newFieldContainer.children[0]
+    if(input.value == ""){
+        return
+    }
+    newFieldContainer.children[0].value = ""
+    container.appendChild(newFieldContainer)
+}
+
+//Delete photo field
+function deletefield(event){
+    const span = event.currentTarget
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length < 2){
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    span.parentNode.remove()
+}
+
+//Select yes or no button
+function toggleSelect(event){
+    document.querySelectorAll('.button-select button')
+    .forEach((button) => {
+        button.classList.remove('active')
+    })
+
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    const input = document.querySelector('[name="open_on_weekends"]')
+    input.value = button.dataset.value
+}
